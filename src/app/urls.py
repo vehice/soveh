7867c95bcf2057c6c41b93d9728fb320333b43fb
Alvaro Alvarez <alv.ale.alvarez@gmail.com>
2018-04-06 18:11:10 -0300

    Maqueteo de los ingresos con carga de datos

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import include, url
 
-from app import views 
-
+from app import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^users$', views.show_users, name='users'),
     url(r'^empresas$', views.show_empresas, name='empresas'),
     url(r'^analisis$', views.show_analisis, name='analisis'),
+    url(r'^ingresos$', views.show_ingresos, name='analisis'),
+    url(r'^ingresos/new$', views.new_ingreso, name='analisis'),
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -3,26 +3,65 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
 from accounts.models import *
+from backend.models import *
+
 
 @login_required
 def home(request):
-    return render(request, "app/home.html",{
-})
+    return render(request, "app/home.html", {})
+
 
 @login_required
 def show_users(request):
     users = User.objects.all()
     usuarios = []
     for usuario in users:
-        aux = {'user': usuario.first_name.title() + " " + usuario.last_name.title(),
-        'email': usuario.email,'id': usuario.id, 'estado': usuario.is_active, 'admin': usuario.is_superuser}
+        aux = {
+            'user':
+            usuario.first_name.title() + " " + usuario.last_name.title(),
+            'email': usuario.email,
+            'id': usuario.id,
+            'estado': usuario.is_active,
+            'admin': usuario.is_superuser
+        }
         usuarios.append(aux)
-    return render(request, 'app/users.html',{'users':usuarios})
+    return render(request, 'app/users.html', {'users': usuarios})
+
 
 @login_required
 def show_empresas(request):
-    return render(request, 'app/empresas.html',{})
+    return render(request, 'app/empresas.html', {})
+
 
 @login_required
 def show_analisis(request):
-    return render(request, 'app/analisis.html',{})
+    return render(request, 'app/analisis.html', {})
+
+
+@login_required
+def show_ingresos(request):
+    return render(request, 'app/ingresos.html', {})
+
+
+@login_required
+def new_ingreso(request):
+
+    species = Specie.objects.all()
+    larvalStages = LarvalStage.objects.all()
+    fixtatives = Fixative.objects.all()
+    waterSources = WaterSource.objects.all()
+    questionReceptionCondition = QuestionReceptionCondition.objects.filter(
+        status='a')
+    exams = Exam.objects.all()
+    organs = Organ.objects.all()
+
+    return render(
+        request, 'app/nuevo_ingreso.html', {
+            'specie_list': species,
+            'larvalStage_list': larvalStages,
+            'fixtative_list': fixtatives,
+            'waterSource_list': waterSources,
+            'questionReceptionCondition_list': questionReceptionCondition,
+            'exam_list': exams,
+            'organ_list': organs
+        })
