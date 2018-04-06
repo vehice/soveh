4bb12fc53a4ff5e468ed137c1c212bb47e9fc489
Alvaro Alvarez <alv.ale.alvarez@gmail.com>
2018-04-06 18:09:59 -0300

    creación de los modelos del backend

--- src/backend/admin.py
+++ src/backend/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Specie, WaterSource, Fixative, Exam, Customer, LarvalStage, \
+QuestionReceptionCondition, EntryForm, AnswerReceptionCondition, Organ
 
-# Register your models here.
+admin.site.register(Specie)
+admin.site.register(Organ)
+admin.site.register(WaterSource)
+admin.site.register(Fixative)
+admin.site.register(Exam)
+admin.site.register(Customer)
+admin.site.register(LarvalStage)
+admin.site.register(QuestionReceptionCondition)
+admin.site.register(AnswerReceptionCondition)
+admin.site.register(EntryForm)
--- src/backend/models.py
+++ src/backend/models.py
@@ -1,3 +1,107 @@
 from django.db import models
+from workflows.models import Flow
 
-# Create your models here.
+
+class Specie(models.Model):
+    name = models.CharField(max_length=250, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class WaterSource(models.Model):
+    name = models.CharField(max_length=250, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class LarvalStage(models.Model):
+    name = models.CharField(max_length=250, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Fixative(models.Model):
+    name = models.CharField(max_length=250, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Exam(models.Model):
+    name = models.CharField(max_length=250, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Organ(models.Model):
+    name = models.CharField(max_length=250, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class QuestionReceptionCondition(models.Model):
+    STATUS = (('a', 'Active'), ('i', 'Inactive'))
+
+    text = models.CharField(max_length=250, null=True, blank=True)
+    status = models.CharField(max_length=1, choices=STATUS)
+
+    def __str__(self):
+        return self.text
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=250, null=True, blank=True)
+    company = models.CharField(max_length=250, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class EntryForm(models.Model):
+    form_id = models.CharField(max_length=250, null=True, blank=True)
+    flow = models.OneToOneField(Flow, null=True, on_delete=models.SET_NULL)
+    specie = models.ForeignKey(Specie, null=True, on_delete=models.SET_NULL)
+
+    watersource = models.ForeignKey(
+        WaterSource,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+    larvalstage = models.ForeignKey(
+        LarvalStage,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+    fixative = models.ForeignKey(
+        Fixative,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+    customer = models.ForeignKey(
+        Customer, null=True, on_delete=models.SET_NULL)
+    questionreceptionconditions = models.ManyToManyField(
+        QuestionReceptionCondition, through='AnswerReceptionCondition')
+    observation = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    sampled_at = models.DateTimeField(null=True, blank=True)
+
+
+class AnswerReceptionCondition(models.Model):
+    ANSWER = (('si', 'Si'), ('no', 'No'), ('n/a', 'N/A'))
+
+    question = models.ForeignKey(
+        QuestionReceptionCondition,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+    entryform = models.ForeignKey(
+        EntryForm,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+    answer = models.CharField(max_length=3, choices=ANSWER)
