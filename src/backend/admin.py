03f61abb93416727ed396d7ffb4ce4a2c42773be
Alvaro <alv.ale.alvarez@gmail.com>
2018-06-11 17:38:58 -0400

    Se agregan modelos a admin

--- src/backend/admin.py
+++ src/backend/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from .models import Specie, WaterSource, Fixative, Exam, Customer, LarvalStage, \
-QuestionReceptionCondition, EntryForm, AnswerReceptionCondition, Organ
+QuestionReceptionCondition, EntryForm, AnswerReceptionCondition, Organ, Slice, \
+Diagnostic, DiagnosticDistribution, DiagnosticIntensity, Pathology, OrganLocation
 
 admin.site.register(Specie)
-admin.site.register(Organ)
 admin.site.register(WaterSource)
 admin.site.register(Fixative)
 admin.site.register(Exam)
@@ -12,3 +12,10 @@ admin.site.register(LarvalStage)
 admin.site.register(QuestionReceptionCondition)
 admin.site.register(AnswerReceptionCondition)
 admin.site.register(EntryForm)
+admin.site.register(Slice)
+admin.site.register(Organ)
+admin.site.register(OrganLocation)
+admin.site.register(Diagnostic)
+admin.site.register(DiagnosticDistribution)
+admin.site.register(DiagnosticIntensity)
+admin.site.register(Pathology)
