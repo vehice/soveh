120ba4b393932001049af3d457900c06cafc3c81
Jose Monagas <Jose@localhost.localdomain>
2021-03-25 10:53:03 -0300

    Created proxy Case and manager CaseManager.
    Case is a proxy model for EntryForm, to add functionality required for
    the lab app without editing the backend app.
    
    CaseManager is used to add some filtering and helper functions to Case queryset.

--- src/lab/models.py
+++ src/lab/models.py
@@ -3,6 +3,41 @@ from django.db import models
 from backend.models import EntryForm, Organ, Unit
 
 
+class CaseManager(models.Manager):
+    """
+    Custom Manager for Case, where it mainly filters all :model:`backend.Entryform`
+    according to it's current :model:`workflows.Form` state. Alongside some helper
+    functions.
+    """
+
+    def get_queryset(self):
+        return super().get_queryset().filter(forms__cancelled=0, forms__form_closed=0)
+
+    def identifications(self):
+        return self.get_queryset().prefetch_related(
+            models.Prefetch("identification_set", to_attr="identifications")
+        )
+
+    def units(self):
+        return self.get_queryset().prefetch_related(
+            models.Prefetch("identification_set", to_attr="identifications"),
+            models.Prefetch("identifications__unit_set", to_attr="units"),
+        )
+
+
+class Case(EntryForm):
+    """
+    Proxy class for :model:`backend.EntryForm`, it's used in the Lab app to filter
+    EntryForms AKA Cases according to their status, and order them by priorities,
+    without touching the other app's implementation.
+    """
+
+    objects = CaseManager()
+
+    class Meta:
+        proxy = True
+
+
 class Cassette(models.Model):
     """
     A Cassette is a plastic unit where organs are put and
