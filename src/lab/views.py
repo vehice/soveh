4f602f70dffa921426ae8582dffc138c7f1038f4
Jose Monagas <Jose@localhost.localdomain>
2021-03-25 14:41:36 -0300

    Create CassetteViews.
    CassetteDetailView is meant to show a single Cassette instance using
    ajax or template.
    
    CassetteBuildView will show the build template and will take post
    request to store new cassettes.
    
    CassetteProcessView will show the process template and will take post
    request to update cassettes with ther processed_at date.

--- src/lab/views.py
+++ src/lab/views.py
@@ -1,1 +1,43 @@
+from django.core import serializers
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.vary import vary_on_headers
+from django.views.generic.detail import SingleObjectMixin
+
+from lab.models import Case
+
+
+class CassetteDetailView(SingleObjectMixin):
+    pass
+
+
+class CassetteBuildView(View):
+    @method_decorator(vary_on_headers("X-Requested-With"))
+    def get(self, request):
+        """
+        Displays list of available :model:`lab.Cassette` to build,
+        from :model:`backend.Unit` where their :model:`backend.EntryForm`.entry_format
+        is either (1, "Tubo"), (2, "Cassette"), (6, "Vivo"), (7, "Muerto").
+        """
+
+        units = Case.objects.units(entry_format__in=[1, 2, 6, 7])
+
+        if request.is_ajax():
+            data = serializers.serialize("json", units)
+
+            return JsonResponse(data, safe=False)
+
+        return render(request, "cassettes/build.html", {"units": units})
+
+    def post(self, request):
+        """
+        Creates a new Cassette storing build_at date and their correlative
+        according to their :model:`backend.Unit` and :model:`backend.EntryForm`
+        """
+        pass
+
+
+class CassetteProcessView(View):
+    pass
