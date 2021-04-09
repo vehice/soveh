00572846a254e75486319e487a6cccee8e59f9b6
Jose Monagas <jose_tadeomong@hotmail.com>
2021-04-09 17:54:46 -0400

    Add CaseDetail. Change cassette_prebuild.
    CaseDetail will return templates for displaying details.
    cassette_prebuild now takes into account rules send in requests,
    this allows for better prototyping of the Cassettes.

--- src/lab/views.py
+++ src/lab/views.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from dateutil.parser import ParserError, parse
 from django.core import serializers
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.paginator import InvalidPage, Paginator
 from django.db.utils import IntegrityError
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
@@ -11,17 +12,18 @@ from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.vary import vary_on_headers
-from django.views.generic.detail import SingleObjectMixin
+from django.views.generic.detail import DetailView
 
-from backend.models import Unit, Organ
+from backend.models import Organ, Unit
 from lab.models import Case, Cassette
 
 
-class CassetteDetailView(SingleObjectMixin):
-    pass
+class CaseDetail(DetailView):
+    model = Case
+    template_name = "cassettes/detail.html"
 
 
-class CassetteBuildView(View):
+class CassetteBuild(View):
     @method_decorator(vary_on_headers("X-Requested-With"))
     def get(self, request):
         """
@@ -39,10 +41,11 @@ class CassetteBuildView(View):
             and :model:`backend.Unit`
 
         **Template**
-        ``lab/build.html``
+        ``lab/cassettes/build.html``
         """
 
         cases = Case.objects.units(entry_format__in=[1, 2, 6, 7])
+
         organs = serializers.serialize("json", Organ.objects.all())
 
         if request.is_ajax():
@@ -60,15 +63,14 @@ class CassetteBuildView(View):
         according to their :model:`backend.Unit` and :model:`backend.EntryForm`
         """
 
-        build_at = request.POST.get("build_at")
-        units = request.POST.get("units")
+        form_request = json.loads(request.body)
+        build_at = form_request["build_at"]
+        units = form_request["units"]
 
         if not units:
             return JsonResponse(
                 {"status": "ERROR", "message": "units is empty"}, status=400
             )
-        else:
-            units = json.loads(units)
 
         if not build_at:
             build_at = datetime.now()
@@ -122,7 +124,7 @@ class CassetteBuildView(View):
         )
 
 
-class CassetteProcessView(View):
+class CassetteProcess(View):
     @method_decorator(vary_on_headers("X-Requested-With"))
     def get(self, request):
         """
@@ -178,28 +180,83 @@ class CassetteProcessView(View):
 @csrf_exempt
 def cassette_prebuild(request):
     """
-    Receives an array of unit ids, and returns an array of `pseudo` Cassettes,
-    grouped by :model:`lab.Case` with their own correlatives.
+    Receives a JSON with 2 keys: `selected` which is an array of unit ids;
+    and `rules` which is in itself another object that dictates how the cassettes are build,
+
+    * rules["unique"]
+        Is an array of unit ids in which all of those units are to be put by themselves
+        in their own Cassette.
+
+    * rules["groups"]
+        Is an array of arrays containing unit ids, in which all of those groups must be put
+        together in their own Cassettes.
+
+    * rules["max"]
+        Is an integer greater than or equal to 0, and any Cassette can't have more organs than this
+        number, unless is 0, then a Cassette can have as many Organs as available.
+
+
+    This returns a JSON containing Cassette prototypes.
     """
-    unit_ids = json.loads(request.body)
-    units = Unit.objects.filter(pk__in=unit_ids).order_by("identification__entryform")
+    items = json.loads(request.body)
+    units_id = items["selected"]
+    rules = items["rules"]
+
+    units = Unit.objects.filter(pk__in=units_id).order_by("identification__entryform")
 
     response = []
+
+    def response_format(unit, organs, count):
+        return {
+            "case": unit.identification.entryform.no_caso,
+            "identification": unit.identification.cage,
+            "unit": unit.correlative,
+            "unit_id": unit.id,
+            "cassette": count,
+            "organs": serializers.serialize("json", unit.organs.all()),
+            "cassette_organs": serializers.serialize("json", organs),
+        }
 
     for unit in units:
         cassette_count = unit.cassettes.count()
         cassette_count = 1 if cassette_count == 0 else cassette_count
-        unit_organs = unit.organs.all()
-        response.append(
-            {
-                "case": unit.identification.entryform.no_caso,
-                "identification": unit.identification.cage,
-                "unit": unit.correlative,
-                "unit_id": unit.id,
-                "cassette": cassette_count,
-                "organs": serializers.serialize("json", unit_organs),
-                "cassette_organs": serializers.serialize("json", unit_organs),
-            }
-        )
+        excludes = []
+
+        if len(rules["uniques"]) > 0:
+            organs = unit.organs.filter(pk__in=rules["uniques"])
+
+            if not organs.count() <= 0:
+                for organ in organs:
+                    response.append(response_format(unit, [organ], cassette_count))
+                    cassette_count += 1
+
+                excludes.extend([pk for pk in rules["uniques"]])
+
+        if len(rules["groups"]) > 0:
+            for group in rules["groups"]:
+                organs = unit.organs.filter(pk__in=group).exclude(pk__in=excludes)
+
+                if not organs.count() <= 0:
+                    response.append(response_format(unit, organs, cassette_count))
+                    cassette_count += 1
+                    excludes.extend([pk for pk in group])
+
+        organs = unit.organs.exclude(pk__in=excludes).order_by("-id")
+
+        if rules["max"] > 0:
+            organs_pages = Paginator(organs, rules["max"], allow_empty_first_page=False)
+
+            for page in organs_pages.page_range:
+                try:
+                    current_page = response_format(
+                        unit, organs_pages.get_page(page), cassette_count
+                    )
+                except InvalidPage:
+                    continue
+                else:
+                    response.append(current_page)
+                    cassette_count += 1
+        elif organs.count() > 0:
+            response.append(response_format(unit, organs, cassette_count))
 
     return HttpResponse(json.dumps(response), content_type="application/json")
