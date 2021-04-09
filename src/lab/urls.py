3111360c0f02166add0ebcbfbccca0bb813c9888
Jose Monagas <jose_tadeomong@hotmail.com>
2021-04-09 17:50:39 -0400

    Create Cassette Detail
    This is displayed using Swal dialog, or just adding it to the page where
    needed. You can access it using CaseView.

--- src/lab/urls.py
+++ src/lab/urls.py
@@ -5,9 +5,8 @@ from lab import views
 
 app_name = "lab"
 urlpatterns = [
-    path("cassette/build", views.CassetteBuildView.as_view(), name="cassette_build"),
-    path(
-        "cassette/process", views.CassetteProcessView.as_view(), name="cassette_process"
-    ),
+    path("case/<int:pk>", views.CaseDetail.as_view(), name="case_detail"),
+    path("cassette/build", views.CassetteBuild.as_view(), name="cassette_build"),
+    path("cassette/process", views.CassetteProcess.as_view(), name="cassette_process"),
     path("cassette/prebuild", views.cassette_prebuild, name="cassette_prebuild"),
 ]
