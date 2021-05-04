f08168edc546c57752c0aab4197466d0137a13fd
Jose Monagas <jose_tadeomong@hotmail.com>
2021-05-04 10:07:41 -0400

    Add read sheet for Cases.
    Add prebuild to urls.

--- src/lab/urls.py
+++ src/lab/urls.py
@@ -19,5 +19,6 @@ urlpatterns = [
     # Slide Routes
     path("slides", views.SlideIndex.as_view(), name="slide_index"),
     path("slides/<int:pk>", views.SlideDetail.as_view(), name="slide_detail"),
+    path("slides/prebuild", views.slide_prebuild, name="slide_prebuild"),
     path("slides/build", views.SlideBuild.as_view(), name="slide_build"),
 ]
