5d7f6e2a007ae5cbc5bc771294400ac567356c9e
Jose Monagas <jose_tadeomong@hotmail.com>
2021-05-10 10:16:15 -0400

    Add tab navigation to pathologist report.

--- src/static/report/js/control-home.js
+++ src/static/report/js/control-home.js
@@ -105,17 +105,10 @@ $(document).ready(function () {
     },
     series: [
       {
+        name: "Porcentajes",
         type: "pie",
-        radius: [20, 110],
-        center: ["25%", "50%"],
-        roseType: "radius",
         label: {
           show: false,
-        },
-        emphasis: {
-          label: {
-            show: true,
-          },
         },
         data: [],
       },
