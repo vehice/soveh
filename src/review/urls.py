d915b35c0d47481d9378e618439109f044fd35de
Jose Monagas <jose_tadeomong@hotmail.com>
2021-05-13 15:43:21 -0400

    Add update_stage view to url

--- src/review/urls.py
+++ src/review/urls.py
@@ -7,4 +7,5 @@ app_name = "review"
 urlpatterns = [
     path("", views.index, name="index"),
     path("state/<int:index>", views.list, name="list"),
+    path("stage/<int:pk>/", views.update_stage, name="stage"),
 ]
