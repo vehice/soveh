57555eaa511abde6243ac5ab8e5835afadacca6e
Jose Monagas <jose_tadeomong@hotmail.com>
2021-05-13 15:44:25 -0400

    Add update_stage function
    This function expects a pk for an Analysis and can update or create a
    stage for it.

--- src/review/views.py
+++ src/review/views.py
@@ -1,15 +1,27 @@
-from django.shortcuts import render
-
-from review.models import Analysis
-from django.http.response import JsonResponse
+from django.contrib.auth.decorators import login_required
 from django.core import serializers
+from django.http.response import JsonResponse
+from django.shortcuts import get_object_or_404, render
+
+from review.models import Analysis, Stage
+import json
 
 
+@login_required
 def index(request):
+    """
+    Renders template which lists multiple :model:`review.Analysis` grouped by
+    their state, allowing the user to move them accross multiple states as necessary.
+    """
     return render(request, "index.html")
 
 
+@login_required
 def list(request, index):
+    """
+    Returns a JSON detailing all :model:`review.Analysis` where their :model:`review.Stage`
+    state has the current index.
+    """
     analysis = None
 
     def serialize_data(queryset):
@@ -24,9 +36,25 @@ def list(request, index):
             )
         return context
 
-    if index in (0, 1, 2, 3):
+    if index in (1, 2, 3, 4):
         analysis = Analysis.objects.stage(index)
     else:
-        analysis = Analysis.objects.pre_report_done()
+        analysis = Analysis.objects.pre_report_done().union(Analysis.objects.stage(0))
 
     return JsonResponse(serialize_data(analysis), safe=False)
+
+
+@login_required
+def update_stage(request, pk):
+    """
+    Updates a :model:`review.Stage`, storing the change in :model:`review.Logbook`.
+    """
+    analysis = get_object_or_404(Analysis, pk=pk)
+    post = json.loads(request.body)
+    state = post["state"]
+
+    stage = Stage.objects.update_or_create(
+        analysis=analysis, defaults={"state": state, "created_by": request.user}
+    )
+
+    return JsonResponse(serializers.serialize("json", [stage[0]]), safe=False)
