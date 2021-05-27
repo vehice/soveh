ef3cadf177ad5c6e9a8bb6baf81efd1c3d77f7b7
Jose Monagas <jose_tadeomong@hotmail.com>
2021-05-27 10:10:52 -0400

    Add is_reviewer to userprofile

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -9,22 +9,29 @@ class Profile(models.Model):
         return self.name
 
 
-LANGUAGE_OPTION = (
-    (1, "ES"),
-    (2, "EN")   
-)
+LANGUAGE_OPTION = ((1, "ES"), (2, "EN"))
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Usuario")
     role = models.CharField(max_length=250, null=True, blank=True)
     rut = models.CharField(max_length=250, null=True, blank=True)
-    phone = models.CharField(max_length=250, null=True, blank=True, verbose_name="Teléfono")
+    phone = models.CharField(
+        max_length=250, null=True, blank=True, verbose_name="Teléfono"
+    )
     state = models.IntegerField(default=1, null=True, blank=True)
-    signature = models.FileField(upload_to='signatures', blank=True, null=True, verbose_name="Firma Digital")
+    signature = models.FileField(
+        upload_to="signatures", blank=True, null=True, verbose_name="Firma Digital"
+    )
     confirmation_code = models.CharField(max_length=250, null=True, blank=True)
-    profile = models.ForeignKey(Profile, null=True, on_delete=models.SET_NULL, verbose_name="Rol")
+    profile = models.ForeignKey(
+        Profile, null=True, on_delete=models.SET_NULL, verbose_name="Rol"
+    )
     is_pathologist = models.BooleanField(default=False, verbose_name="¿Es patólogo?")
-    language = models.IntegerField(default=1, choices=LANGUAGE_OPTION, verbose_name="Lenguage")
+    is_reviewer = models.BooleanField(default=False, verbose_name="¿Es revisor?")
+    language = models.IntegerField(
+        default=1, choices=LANGUAGE_OPTION, verbose_name="Lenguage"
+    )
 
     def __str__(self):
         return self.profile.name
@@ -36,7 +43,6 @@ class UserProfile(models.Model):
             self.user.save()
 
         super(UserProfile, self).save(*args, **kwargs)
-   
 
     class Meta:
         verbose_name = "Perfil de Usuario"
