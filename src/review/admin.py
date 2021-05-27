17cd89e1578a8dcbc432023f117b2fd551aa92f2
Jose Monagas <jose_tadeomong@hotmail.com>
2021-05-27 11:42:05 -0400

    Fix admin not showing deactivated model instances.

--- src/review/admin.py
+++ src/review/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from review.models import Recipient, MailList
 
-admin.site.register(Recipient)
-admin.site.register(MailList)
+
+@admin.register(Recipient)
+class RecipientAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        return self.model.items.get_queryset()
+
+
+@admin.register(MailList)
+class MailListAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        return self.model.items.get_queryset()
