76017499c2b5327433d76690f153558559ae1207
Jose Monagas <jose_tadeomong@hotmail.com>
2021-08-10 11:49:53 -0400

    Add post logic to store processing date

--- src/static/lab/js/cassette-process.js
+++ src/static/lab/js/cassette-process.js
@@ -1,68 +1,122 @@
-const dlgArmarCassette = new bootstrap.Modal(
-  document.getElementById("dlgProcess")
+const dlgProcesarCassette = new bootstrap.Modal(
+    document.getElementById("dlgProcess")
 );
 
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+        var cookies = document.cookie.split(";");
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === name + "=") {
+                cookieValue = decodeURIComponent(
+                    cookie.substring(name.length + 1)
+                );
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
 $(document).ready(function () {
-  $("#datatable").DataTable({
-    dom: "Bfrtip",
+    const tableList = $("#datatable").DataTable({
+        dom: "Bfrtip",
 
-    buttons: [
-      {
-        text: "Seleccionar todos",
-        action: function () {
-          tableIndex
-            .rows({
-              page: "current",
-            })
-            .select();
+        buttons: [
+            {
+                text: "Seleccionar todos",
+                action: function () {
+                    tableIndex
+                        .rows({
+                            page: "current",
+                        })
+                        .select();
+                },
+            },
+
+            {
+                text: "Deseleccionar todos",
+                action: function () {
+                    tableIndex
+                        .rows({
+                            page: "current",
+                        })
+                        .deselect();
+                },
+            },
+        ],
+
+        columnDefs: [{ targets: [0, 2], width: "1rem" }],
+
+        select: {
+            style: "multi",
         },
-      },
 
-      {
-        text: "Deseleccionar todos",
-        action: function () {
-          tableIndex
-            .rows({
-              page: "current",
-            })
-            .deselect();
+        paging: false,
+
+        oLanguage: {
+            sUrl:
+                "https://cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json",
         },
-      },
-    ],
-
-    select: {
-      style: "multi",
-    },
-
-    paging: false,
-
-    rowsGroup: [1, 2, 3],
-
-    oLanguage: {
-      sUrl: "https://cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json",
-    },
-  });
-
-  $("#btnProcess").on("click", () => {
-    dlgArmarCassette.show();
-    $("#datatable")
-      .DataTable()
-      .rows({ selected: true })
-      .data()
-      .each((element) => {
-        console.log({ element });
-      });
-  });
-
-  $(".detailTrigger").click(function (e) {
-    e.preventDefault();
-    const url = $(e.target).attr("href");
-    $.get(url, function (data, textStatus) {
-      console.log(data);
-      Swal.fire({
-        html: data,
-        width: "80%",
-      });
     });
-  });
+
+    $(".detailTrigger").click(function (e) {
+        e.preventDefault();
+        const url = $(e.target).attr("href");
+        $.get(url, function (data, textStatus) {
+            console.log(data);
+            Swal.fire({
+                html: data,
+                width: "80%",
+            });
+        });
+    });
+
+    $("#btnProcess").click(() => {
+        const processedAt = $("#processAt").val();
+
+        if (!processedAt) {
+            toastr.error("Fecha de procesado no puede estar vacia.");
+            return;
+        }
+
+        let selectedCassettesPk = [];
+        tableList
+            .rows({ selected: true })
+            .data()
+            .each((test) => {
+                selectedCassettesPk.push(test[0]);
+            });
+
+        $.ajax(Urls["lab:cassette_process"](), {
+            data: JSON.stringify({
+                processed_at: processedAt,
+                cassettes: selectedCassettesPk,
+            }),
+
+            method: "POST",
+
+            headers: {
+                "X-CSRFToken": getCookie("csrftoken"),
+            },
+
+            contentType: "application/json; charset=utf-8",
+
+            success: (data, textStatus) => {
+                Swal.fire({
+                    icon: "success",
+                    title: "Guardado",
+                }).then(() => {
+                    location.reload();
+                });
+            },
+            error: (xhr, textStatus, error) => {
+                Swal.fire({
+                    icon: "error",
+                });
+            },
+        });
+    });
 });
